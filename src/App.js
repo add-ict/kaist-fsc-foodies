071a3ea738d27758eb8614325b342fe28241e871
heejbin <heejbin@kaist.ac.kr>
2022-05-18 02:45:06 +0900

    import Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Container from '@mui/material/Container';
+import Header from './components/Header';
 
 function App() {
   return (
-    <h1>응애</h1>
+    <Container maxWidth="sm">
+      <Header></Header>
+      <div style={{width:"100%", height:"100vh", backgroundColor:"red"}}></div>
+    </Container>
   );
 }
 
