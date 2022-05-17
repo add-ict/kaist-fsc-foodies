bef600208a2210a50664bbc038044e3c24b5a9cf
heejbin <heejbin@kaist.ac.kr>
2022-05-18 03:00:19 +0900

    lang button

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import Container from '@mui/material/Container';
 import Header from './components/Header';
+import data from './SubscribeDB';
+import React,{useState} from 'react';
 
 function App() {
+  const [lang, setLang]=useState("ko");
+  function changeLang(){
+    if (lang==="ko")
+      setLang("en");
+    else setLang("ko");
+  }
   return (
     <Container maxWidth="sm">
-      <Header></Header>
-      <div style={{width:"100%", height:"100vh", backgroundColor:"red"}}></div>
+      <Header lang={lang} changeLang={changeLang}></Header>
+      <div style={{width:"100%", height:"100vh", backgroundColor:"red"}}>
+        {JSON.stringify(data.tags)}
+      </div>
     </Container>
   );
 }
